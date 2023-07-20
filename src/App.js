b6a25585b8810a6ff9b2c17e52b411a8e06af46d
Moises Pahati <moisespahati@gmail.com>
2023-07-20 14:19:18 +0800

    add handleSplitBill function

--- src/App.js
+++ src/App.js
@@ -49,6 +49,10 @@ export default function App() {
     setShowAddFriend(false);
   }
 
+  function handleSplitBill(value) {
+    console.log(value);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -66,7 +70,10 @@ export default function App() {
 
 
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend}/>}
+      {selectedFriend && <FormSplitBill
+        selectedFriend={selectedFriend}
+        onSplitBill={handleSplitBill}
+      />}
     </div>
   );
 }
@@ -160,13 +167,22 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill({selectedFriend}) {
+function FormSplitBill({selectedFriend, onSplitBill}) {
   const [bill, setBill] = useState('');
   const [paidByUser, setPaidByUser] = useState('');
+  const paidByFriend = bill ? bill - paidByUser: "";
   const [whoIsPaying, setWhoIsPaying] = useState("user")
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill()
+
+  }
   
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split a bill with { selectedFriend.name }</h2>
 
       <label>💰 Bill value</label>
@@ -180,11 +196,17 @@ function FormSplitBill({selectedFriend}) {
       <input
         type="text"
         value={paidByUser}
-        onChange={e => setPaidByUser(Number(e.target.value))}
+        onChange={e => setPaidByUser(
+          (Number(e.target.value) > bill || 
+          isNaN(Number(e.target.value))) ?
+          paidByUser : Number(e.target.value)
+        )}
       />
 
       <label>🤼 {selectedFriend.name}'s' expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled
+              value={paidByFriend}
+      />
 
       <label>💸 Who is paying the bill</label>
       <select
