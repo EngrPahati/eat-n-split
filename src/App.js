a3056e6f31ed6e475a1182d14f2c97564795ef82
Moises Pahati <moisespahati@gmail.com>
2023-07-20 10:21:36 +0800

    add useState hook on image input

--- src/App.js
+++ src/App.js
@@ -94,16 +94,24 @@ function Friend({friend}) {
 
 function FormAddFriend() {
   const [name, setName] = useState('');
-  // const [image, setImage] = useState('');
+  const [image, setImage] = useState('');
 
 
   return (
     <form className="form-add-friend">
       <label>👨🏽‍🤝‍👨🏽Friend Name</label>
-      <input type='text' value={name} onChange={e => setName(e.target.value)} />
+      <input
+        type='text'
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
 
       <label>👩‍🏫 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        setImage={e => setImage(e.target.value)}
+      />
       
       <Button>Add</Button>
     </form>
